1e0d97c91a864807321176a1417836a217cddccb
Daniil <dap100@yandex.ru>
2018-04-08 10:53:54 +0300

    Commit

---
+++ test_api.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<script src="//vk.com/js/api/openapi.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+
+
+function Login() {
+	
+	 VK.init({apiId: '6440328'});
+	   //авторизация VK
+	   	   	   
+		    VK.Auth.login(function(response){
+		      if (response.session) {			
+			      
+			   
+			  };
+		     },/+262148);
+         
+	};
+	
+	
+function Post() {
+	
+	 VK.Api.call("wall.post", {owner_id: '-85069881', message: 'test', attachments: 'photo-85069881_359163524', v:'5.73'}, function (data) {
+		 
+		 
+	 });
+         
+	};
+
+
+</script>
+
+</head>
+<body onload="Login()">
+
+<button type="button" onclick="Post()">x</button>
+
+</body>
+
+</html>
